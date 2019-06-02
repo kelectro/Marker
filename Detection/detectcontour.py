fd5aae7dd176acc5063e53c101aa532283e8d081
Thanasispapam <thanasis.papamarinos@gmail.com>
2019-06-02 15:50:46 +0200

    ante gamhsou

--- Detection/detectcontour.py
+++ Detection/detectcontour.py
@@ -27,12 +27,13 @@ import numpy as np
 import matplotlib as plt
 import imutils
 
-cam = cv2.VideoCapture(0)
+cam = cv2.VideoCapture('file4.mp4')
 # keep looping
 while True:
 	# grab the current frame and initialize the status text
 	(grabbed, frame) = cam.read()
-	status = "No Targets"
+	frame = cv2.resize(frame, (1080, 720))
+	status = "No markers"
 	
 	# convert the frame to grayscale, blur it, and detect edges
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -42,7 +43,7 @@ while True:
 	cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
 	cv2.CHAIN_APPROX_SIMPLE)
 	cnts = imutils.grab_contours(cnts)
-	
+
 	# loop over the contours
 	p = 0
 	for c in cnts:
@@ -73,8 +74,9 @@ while True:
 			if keepDims and keepSolidity and keepAspectRatio:
 				# draw an outline around the target and update the status
 				# text
-				cv2.drawContours(frame, [approx], -1, (0, 0, 255), 4)
-				status = "Target(s) Acquired"
+				mask = np.zeros(frame.shape[:2], np.uint8)
+				cv2.drawContours(frame, [approx], -1, (0, 255, 255), 4)
+				status = "gotcha"
  
 				# compute the center of the contour region and draw the
 				# crosshairs
@@ -82,16 +84,16 @@ while True:
 				(cX, cY) = (int(M["m10"] // M["m00"]), int(M["m01"] // M["m00"]))
 				(startX, endX) = (int(cX - (w * 0.15)), int(cX + (w * 0.15)))
 				(startY, endY) = (int(cY - (h * 0.15)), int(cY + (h * 0.15)))
-				cv2.line(frame, (startX, cY), (endX, cY), (0, 0, 255), 3)
-				cv2.line(frame, (cX, startY), (cX, endY), (0, 0, 255), 3)
-				print (cX, cY)
+				cv2.line(frame, (startX, cY), (endX, cY), (255, 255, 0), 3)
+				cv2.line(frame, (cX, startY), (cX, endY), (255, 255, 0), 3)
+				#print ("The center of the marker is: ", cX, ", ", cY)
+				cv2.putText(frame, str(cX) + "," + str(cY) , (cX + 10, cY + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 					
 	# draw the status text on the frame
-	cv2.putText(frame, status, (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-		(0, 0, 255), 2)
+	cv2.putText(frame, status, (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
 	# show the frame and record if a key is pressed
-	cv2.imshow("Frame", frame)
-	key = cv2.waitKey(1) & 0xFF
+	cv2.imshow("Video", frame)
+	key = cv2.waitKey(50) & 0xFF
  
 	# if the 'q' key is pressed, stop the loop
 	if key == ord("q"):
